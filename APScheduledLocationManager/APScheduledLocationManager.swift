ae032582673458d01612723f900f697dbdba0749
calvinalvin <calvinoh@gmail.com>
2017-03-27 11:59:47 -0700

    Fix typo in func name `stoptUpdatingLocation` -> `stopUpdatingLocation`

--- APScheduledLocationManager/APScheduledLocationManager.swift
+++ APScheduledLocationManager/APScheduledLocationManager.swift
@@ -79,7 +79,7 @@ public class APScheduledLocationManager: NSObject, CLLocationManagerDelegate {
         startLocationManager()
     }
     
-    public func stoptUpdatingLocation() {
+    public func stopUpdatingLocation() {
         
         isRunning = false
         
